951b06f2878bf802882bd438ccf96090bc46aae1
Eashan Ashrafulzaman <eashan.ashrafulzaman@live.com>
2019-06-27 15:37:14 +0600

    Initial commit

---
+++ Palmzer/Controller/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Palmzer
+//
+//  Created by Eashan on 6/25/19.
+//  Copyright © 2019 The Wave App. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
---
+++ Palmzer/Controller/SignupVC.swift
@@ -0,0 +1,16 @@
+//
+//  SignupVC.swift
+//  Palmzer
+//
+//  Created by Parves Kawser on 26/6/19.
+//  Copyright © 2019 The Wave App. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
